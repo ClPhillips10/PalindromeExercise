271ec0ea5a511d3677c62515f59d9188c1da3010
ClPhillips10 <Lakephillips3232@gmail.com>
2023-06-22 07:21:25 -0500

    Complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PalindromeExercise
+	
+{
+	public class WordSmith
+	{
+		public WordSmith()
+		{
+		}
+		public bool IsAPalindrome(string str1)
+		{
+			var reversed = "";
+			for (int i = str1.Length -1; i >= 0; i--)
+			{
+				reversed += str1[i];
+			}
+			if (reversed.ToLower() == str1.ToLower())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,21 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        
+        [Theory]
+        [InlineData("Racecar", true)]
+        [InlineData("mom", true)]
+        [InlineData("Hello", false)]
+        public void BoolPalindrome(string str1 , bool expected)
         {
-
+            var TestCase = new WordSmith();
+            bool PalindromeActual = TestCase.IsAPalindrome(str1);
+            Assert.Equal(expected, PalindromeActual);
         }
     }
 }
